8c44aab18f5cef1c9cbccf829d6fe142f683519a
Wankupi <2893353848@qq.com>
2021-05-01 15:54:03 +0800

    单独提出 card的css; 等

--- static/js/index.js
+++ static/js/index.js
@@ -1,39 +1,52 @@
-// 拉取 json 以获取文章列表和友链列表
-function putArticleHtml(Arts) {
-  let art_html = "";
-  for (let i = 1; i < Arts.length && i <= 9; ++i) {
-    let art = Arts[Arts.length - i];
-    art_html += `<div class="card">` +
-      `<h3 class="art-title"><a href="/Article/${art.path}" target="_blank">${art.title}</a></h3>` +
-      `<div class="art-outline">${art.outline}</div>` +
-      `</div>\n`;
+(function () {
+  function $(s) { return document.querySelector(s); }
+
+  // 拉取 json 以获取文章列表和友链列表
+  function putArticleHtml(Arts) {
+    function DateToHTML(d) {
+      if ((typeof d) != "string") return "";
+      let t = new Date(d);
+      return `${t.getFullYear()}-${(t.getMonth() + 1).toString().padStart(2, '0')}-${t.getDate().toString().padStart(2, '0')} ` +
+        `${t.getHours().toString().padStart(2, "0")}:${t.getMinutes().toString().padStart(2, "0")}`;
+    }
+
+    let art_html = "";
+    for (let i = 1; i < Arts.length && i <= 9; ++i) {
+      let art = Arts[Arts.length - i];
+      art.type = art.path.split("/")[0];
+      art_html += `<div class="card">` +
+        `<h3 class="art-title"><a href="/Article/${art.path}">${art.title}</a></h3>` +
+        `<header class="art-meta">` + (
+          `<span class="art-type">${art.type}</span>` +
+          `<span class="art-date">${DateToHTML(art.date)}</span>`
+        ) + `</header>` +
+        `<div class="art-outline">${art.outline}</div>` +
+        `</div>`;
+    }
+    if (Arts.length > 9)
+      art_html += `<div class="card more-art"><h3 class="art-title"><a href="/Article" target="_blank">更多</a></h3></div>`
+    $("#articles").innerHTML = art_html;
+    if (MathJax.typesetPromise) MathJax.typesetPromise(); else MathJax.startup.typeset = true;
   }
-  if (Arts.length > 9)
-    art_html += `<div class="card more-art"><h3 class="art-title"><a href="/Article" target="_blank">更多</a></h3></div>`
-  $("#articles").html(art_html);
-  if (MathJax.typesetPromise) MathJax.typesetPromise(); else MathJax.startup.typeset = true;
-}
 
-fetch("/Article/list.json").then(response => response.json()).then(putArticleHtml);
+  fetch("/Article/list.json").then(response => response.json()).then(putArticleHtml);
 
-// 一言
-function putHitokoto() {
-  fetch("https://v1.hitokoto.cn?c=i").then(response => response.json())
-    .then(function (myJson) {
-      $("#hitokotoText").html(myJson.hitokoto);
-      $("#hitokotoWho").text(myJson.from_who);
-    });
-}
-putHitokoto();
-$("#hitokoto img").on("click", putHitokoto);
+  // 一言
+  function putHitokoto() {
+    fetch("https://v1.hitokoto.cn?c=i").then(response => response.json())
+      .then(function (myJson) {
+        $("#hitokotoText").innerText = myJson.hitokoto;
+        $("#hitokotoWho").innerText = myJson.from_who;
+      });
+  }
+  putHitokoto();
+  $("#hitokoto img").addEventListener("click", putHitokoto);
 
-var Top_Panel = $("#top-panel")[0];
-document.addEventListener("scroll", function (event) {
-  let height = document.documentElement.clientHeight;
-  let H = document.documentElement.scrollTop;
-  if (H >= height) Top_Panel.classList.add("show");
-  else Top_Panel.classList.remove("show");
-});
-
-$(".scroll-down").on("click", function () { $("html,body").animate({ scrollTop: $("main").offset().top }, 1000); });
-$(".back-top").on("click", e => { $("html,body").animate({ scrollTop: 0 }, 1000); });
+  var Top_Panel = $("#top-panel");
+  document.addEventListener("scroll", function (event) {
+    let height = document.documentElement.clientHeight;
+    let H = document.documentElement.scrollTop;
+    if (H >= height) Top_Panel.classList.add("show");
+    else Top_Panel.classList.remove("show");
+  });
+})()
--- static/js/loads.js
+++ static/js/loads.js
@@ -1,41 +1,60 @@
-var MathJax = { startup: { typeset: true, output: 'chtml' }, tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] }, options: { enableMenu: false } };
+let isLocal = (location.hostname == "localhost" || location.hostname == "127.0.0.1");
+let isMainPage = (location.pathname == "/");
 
-document.title += " | Wankupi";
-function addCSS_wankupi(url) { let lnk = document.createElement("link"); lnk.rel = "stylesheet"; lnk.href = url; document.head.appendChild(lnk); }
-
-addCSS_wankupi("/static/css/highlight.css");
+var MathJax = { startup: { typeset: (!isMainPage), output: 'chtml' }, tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] }, options: { enableMenu: false } };
 
 (function () {
   let d = document.createElement("script");
-  d.src = "/static/js/highlight/highlight.pack.js";
-  d.async = true;
-  d.addEventListener("load", e => { hljs.initHighlighting(); });
-  document.head.appendChild(d);
-})();
-
-(function () {
-  let d = document.createElement("script");
-  d.src = (location.hostname == "localhost" ? "/static/js/MathJax/es5/tex-chtml.js" : "https://cdn.jsdelivr.net/npm/mathjax/es5/tex-chtml.js");
+  d.id = "MathJax-script";
+  d.src = (isLocal ? "/static/js/MathJax/es5/tex-chtml.js" : "https://cdn.jsdelivr.net/npm/mathjax/es5/tex-chtml.js");
   d.async = true;
   document.head.appendChild(d);
 })();
 
-(function () {
-  function loadValine() {
-    new Valine({
-      el: '#Comment',
-      app_id: '0fXMSbCD7SIxO3z7u5j1VjkL-gzGzoHsz',
-      app_key: 'eIi8ECHYotfQqPB4AQRI6nfi',
-      placeholder: '写下你的精彩评论！',
-      lang: 'zh-CN',
-      visitor: false,
-      recordIP: true,
-      requiredFields: ['nick']
+if (isMainPage) {
+  (function () {
+    let d = document.createElement("script");
+    d.id = "jquerj-script";
+    d.src = (isLocal ? "/static/js/jquery/jquery.min.js" : "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js");
+    d.addEventListener("load", (e) => {
+      $(".scroll-down").on("click", function () { $("html,body").animate({ scrollTop: $("main").offset().top }, 1000); });
+      $(".back-top").on("click", e => { $("html,body").animate({ scrollTop: 0 }, 1000); });
     });
-  }
-  let d = document.createElement("script");
-  d.src = "https://cdn.jsdelivr.net/npm/valine/dist/Valine.min.js";
-  d.async = true;
-  d.addEventListener("load", loadValine);
-  document.head.appendChild(d);
-})();
+    document.head.appendChild(d);
+  })();
+}
+else {
+  document.title += " | Wankupi";
+
+  function addCSSfile(url) { let lnk = document.createElement("link"); lnk.rel = "stylesheet"; lnk.href = url; document.head.appendChild(lnk); }
+
+  addCSSfile("/static/css/highlight.css");
+
+  (function () {
+    let d = document.createElement("script");
+    d.src = "/static/js/highlight/highlight.pack.js";
+    d.async = true;
+    d.addEventListener("load", e => { hljs.initHighlighting(); });
+    document.head.appendChild(d);
+  })();
+
+  (function () {
+    function loadValine() {
+      new Valine({
+        el: '#Comment',
+        app_id: '0fXMSbCD7SIxO3z7u5j1VjkL-gzGzoHsz',
+        app_key: 'eIi8ECHYotfQqPB4AQRI6nfi',
+        placeholder: '写下你的精彩评论！',
+        lang: 'zh-CN',
+        visitor: false,
+        recordIP: true,
+        requiredFields: ['nick']
+      });
+    }
+    let d = document.createElement("script");
+    d.src = "https://cdn.jsdelivr.net/npm/valine/dist/Valine.min.js";
+    d.async = true;
+    d.addEventListener("load", loadValine);
+    document.head.appendChild(d);
+  })();
+}
