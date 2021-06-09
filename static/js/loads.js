fb28d6caed03e8f1d2884c624289c280fb61da82
Wankupi <2893353848@qq.com>
2021-06-09 12:03:51 +0800

    访问记录

--- static/js/loads.js
+++ static/js/loads.js
@@ -3,58 +3,60 @@ let isMainPage = (location.pathname == "/");
 
 var MathJax = { startup: { typeset: (!isMainPage), output: 'chtml' }, tex: { inlineMath: [['$', '$'], ['\\(', '\\)']] }, options: { enableMenu: false } };
 
-(function () {
+function loadJs(src, config, func) {
   let d = document.createElement("script");
-  d.id = "MathJax-script";
-  d.src = (isLocal ? "/static/js/MathJax/es5/tex-chtml.js" : "https://cdn.jsdelivr.net/npm/mathjax/es5/tex-chtml.js");
-  d.async = true;
+  d.src = src;
+  for (i in config) d.setAttribute(i, config[i]);
+  d.addEventListener("load", (e) => { (typeof (func) == "function") && func(e); e.target.remove(); });
   document.head.appendChild(d);
-})();
+}
+
+function loadCss(url) {
+  let lnk = document.createElement("link");
+  lnk.rel = "stylesheet";
+  lnk.type = "text/css";
+  lnk.href = url;
+  document.head.appendChild(lnk);
+}
+
+loadJs(isLocal ? "/static/js/MathJax/es5/tex-chtml.js" : "https://cdn.jsdelivr.net/npm/mathjax/es5/tex-chtml.js", { id: "MathJax-script", async: "" });
+
+if (!isLocal) {
+  loadJs("http://cdn.jsdelivr.net/npm/leancloud-storage@3/dist/av-min.js", { async: "" }, (e) => {
+    AV.init({ appId: "0fXMSbCD7SIxO3z7u5j1VjkL-gzGzoHsz", appKey: "eIi8ECHYotfQqPB4AQRI6nfi", serverURL: "https://0fxmsbcd.lc-cn-n1-shared.com" });
+    const r = new AV.Object("AccessRecords");
+    r.set("url", location.pathname);
+    r.set("hostname", location.hostname);
+    if (AV.User.current()) r.set("User", AV.User.current().attributes.username);
+    r.set("ua", navigator.appVersion);
+    r.save();
+  });
+}
 
 if (isMainPage) {
-  (function () {
-    let d = document.createElement("script");
-    d.id = "jquerj-script";
-    d.src = (isLocal ? "/static/js/jquery/jquery.min.js" : "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js");
-    d.addEventListener("load", (e) => {
-      $(".scroll-down").on("click", function () { $("html,body").animate({ scrollTop: $("main").offset().top }, 1000); });
-      $(".back-top").on("click", e => { $("html,body").animate({ scrollTop: 0 }, 1000); });
-    });
-    document.head.appendChild(d);
-  })();
+  loadJs(isLocal ? "/static/js/jquery/jquery.min.js" : "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js", { id: "jquery-script" }, () => {
+    $(".scroll-down").on("click", function () { $("html,body").animate({ scrollTop: $("main").offset().top }, 1000); });
+    $(".back-top").on("click", e => { $("html,body").animate({ scrollTop: 0 }, 1000); });
+  });
 }
 else {
   document.title += " | Wankupi";
 
-  function addCSSfile(url) { let lnk = document.createElement("link"); lnk.rel = "stylesheet"; lnk.href = url; document.head.appendChild(lnk); }
+  loadCss("/static/css/highlight.css");
 
-  addCSSfile("/static/css/highlight.css");
+  loadJs("/static/js/highlight/highlight.pack.js", { async: true }, () => { hljs.initHighlighting(); });
 
-  (function () {
-    let d = document.createElement("script");
-    d.src = "/static/js/highlight/highlight.pack.js";
-    d.async = true;
-    d.addEventListener("load", e => { hljs.initHighlighting(); });
-    document.head.appendChild(d);
-  })();
-
-  (function () {
-    function loadValine() {
-      new Valine({
-        el: '#Comment',
-        app_id: '0fXMSbCD7SIxO3z7u5j1VjkL-gzGzoHsz',
-        app_key: 'eIi8ECHYotfQqPB4AQRI6nfi',
-        placeholder: '写下你的精彩评论！',
-        lang: 'zh-CN',
-        visitor: false,
-        recordIP: true,
-        requiredFields: ['nick']
-      });
-    }
-    let d = document.createElement("script");
-    d.src = "https://cdn.jsdelivr.net/npm/valine/dist/Valine.min.js";
-    d.async = true;
-    d.addEventListener("load", loadValine);
-    document.head.appendChild(d);
-  })();
+  loadJs("https://cdn.jsdelivr.net/npm/valine/dist/Valine.min.js", { async: "" }, () => {
+    new Valine({
+      el: '#Comment',
+      app_id: '0fXMSbCD7SIxO3z7u5j1VjkL-gzGzoHsz',
+      app_key: 'eIi8ECHYotfQqPB4AQRI6nfi',
+      placeholder: '写下你的精彩评论！',
+      lang: 'zh-CN',
+      visitor: true,
+      recordIP: true,
+      enableQQ: true,
+      requiredFields: ['nick', 'mail']
+    });
+  });
 }
